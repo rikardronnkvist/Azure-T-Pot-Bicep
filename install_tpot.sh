5c2aeae99406a9d04eaf2588d39535ca191c4e93
Rikard Ronnkvist <rikard@ronnkvist.nu>
2022-08-21 15:18:10 +0200

    Compose

--- install_tpot.sh
+++ install_tpot.sh
@@ -9,10 +9,10 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 
 # T-Pot installation
-sudo apt install git -y
+sudo apt-get install git -y
 
 # Clone T-Pot
 git clone https://github.com/telekom-security/tpotce
